db65c4b61d31b46095b40e1836fa54b3ec249403
Thomas Brawner <thomasbrawner@gmail.com>
2015-12-09 09:18:09 -0700

    Add specification & formatting methods, function for optimizing ridge penalty

--- dissertation.py
+++ dissertation.py
@@ -7,14 +7,34 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils import resample 
 
 
-class DissertationData(object): 
-    def __init__(self, dframe, depvar): 
-        self.y = dframe.pop(depvar).values
-        self.X = dframe.values 
+class DataFormatter(object): 
+    def __init__(self, dframe, depvar):
+        self.y = dframe.pop(depvar).values 
         self.feature_names = dframe.columns.tolist() 
         if 'year' in self.feature_names:
             self.years = dframe['year'].values 
 
-    def scale(self): 
-        return StandardScaler().fit_transform(self.X)
+    def set_specification(self, lag, factors=None): 
+        lag = [lag]
+        regimes = ['military', 'personal', 'party', 'institutions']
+        controls = ['duration', 'gdppc', 'growth', 'resource']
+        dummies = []
+        if factors is not None: 
+            for factor in factors: 
+                dummies += [col for col in self.feature_names if factor in col]
+        self.specification = lag + regimes + controls + dummies 
+
+    def format_features(self, scale=False): 
+        if not hasattr(self, 'specification'): 
+            raise Exception('Need to set specification before formatting features')     
+        if scale: 
+            self.X = StandardScaler().fit_transform(dframe[self.specification].values)
+        else: 
+            self.X = dframe[self.specification].values
+
+def optimal_l2(X, y): 
+    # Find the optimal level of L2 regularization 
+    logit = LogisticRegressionCV(Cs=50, cv=10)
+    logit.fit(X, y)
+    return logit.C_ 
 
